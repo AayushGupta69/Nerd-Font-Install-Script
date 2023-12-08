78b735a3a3f8ae4976280acb1f98f17f72dbf23b
Aayush Gupta <72346589+AayushGupta69@users.noreply.github.com>
2023-12-08 16:56:54 +0530

    Updated firacode.sh

--- firacode.sh
+++ firacode.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 # install FiraCode Nerd Font
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 unzip FiraCode.zip -d ~/.fonts
 fc-cache -fv
 echo "Done!"
